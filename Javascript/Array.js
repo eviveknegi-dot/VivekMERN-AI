8b29880591a3d28884949399717d7c6eb369ec10
viveknegi <e.viveknegi@gmail.com>
2026-08-01 21:59:52 +0530

    Javascript folder and fiels added also display.html web page added

---
+++ Javascript/Array.js
@@ -0,0 +1,37 @@
+// let array=["product-one","product-two","product-three","product-four"];
+// console.log(Object.keys(array));
+// array.length = 10; length given to array is will icrease or decrase value in original array.
+// console.log(array.length);
+
+// for each loop empty slot dont itterate.
+
+// array.forEach((item,index,array)=>{
+
+//     console.log(item,index,array); // for each can show three things like element array, arr[index], arr[];
+
+// })
+
+
+// console.log(array.reverse());
+
+// filter create shallow copy.
+const words = ["spray", "elite", "exuberant", "destruction", "present"];
+
+// const result = words.filter((word) => word.includes("t"));
+
+// console.log(result); // 
+// we can put a function inside the filter aregumnet. like => 
+
+// function isBigEnough(value) {
+//   return value >= 10;
+// }
+// const filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+
+
+// Map 
+// const array = [1, 4, 9, 16];
+
+// // Pass a function to map
+// const mapped = array.map((x) => x * 2);
+
+// console.log(mapped);
